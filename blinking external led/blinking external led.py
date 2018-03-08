3d3b5f1e0c8167ba74c2cce4c682111c769906b6
Adrián Villanueva <thexiao77@users.noreply.github.com>
2018-03-08 19:13:37 +0100

    hotfix

--- blinking external led/blinking external led.py
+++ blinking external led/blinking external led.py
@@ -3,7 +3,7 @@ import pyfirmata
 board = pyfirmata.Arduino("COM3")
 
 print('connected to: ' + str(board))
-pin = board.get_pin('d:3:p')
+pin = board.get_pin('d:3:p')  # Digital : pin:3 : p:PWM
 print('connected to: ' + str(pin))
 while True:
     pin.write(0)
