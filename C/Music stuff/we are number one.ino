974cae08a9a8310a037a13f36ae563d7b8fd2a22
Adrián Villanueva <16082421+adrianvillanueva997@users.noreply.github.com>
2018-05-22 09:17:00 +0300

    added more stuff :computer:
    Added a new C directory and added a directoy for Python, now everything is more organised (i hope)

---
+++ C/Music stuff/we are number one.ino
@@ -0,0 +1,58 @@
+/*
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+*/
+
+const int fn = 400;
+const int hn = 200;
+const int qn = 100;
+const int f5 = 698;
+const int c6 = 1047;
+const int b5 = 988;
+const int gh5 = 831;
+const int ch6 = 1109;
+const int dh6 = 1245;
+
+int song[] = {
+  f5,fn + hn,
+  c6,hn,
+  b5,qn,
+  c6,qn,
+  b5,qn,
+  c6,qn,
+  b5,hn,
+  c6,hn,
+  gh5,fn,
+  f5,fn + hn,
+  f5,hn,
+  gh5,hn,
+  c6,hn,
+  ch6,fn,
+  gh5,fn,
+  ch6,fn,
+  dh6,fn,
+  c6,hn,
+  ch6,hn,
+  c6,hn,
+  ch6,hn,
+  c6,fn
+  };
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+for (int i = 0; i < (sizeof(song)/sizeof(int)); i = i + 2){
+  tone(8, song[i]);
+  delay(song[i + 1]);
+}
+
+noTone(8);
+delay(1000);
+
+}
